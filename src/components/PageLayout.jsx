c13bf0639f5dbd67fc32ce6e5fa94e10bf9ffa5c
Sero <kmcaltiz@gmail.com>
2024-12-01 23:47:03 +0800

    adjust screen size

--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const PageLayout = ({ children }) => {
-  return <section className="max-w-4xl mx-aut">{children}</section>;
+  return (
+    <section className="max-w-4xl mx-auto p-2 min-h-screen h-screen">{children}</section>
+  );
 };
 
 export default PageLayout;
