8aa3c5a8e84bdf440ba7f5869dcee28ecd98cd8f
Sero <kmcaltiz@gmail.com>
2024-12-01 23:52:46 +0800

    create login page and form

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+
+const LoginForm = () => {
+  const [formData, setFormData] = useState({ email: "", password: "" });
+
+  const handleDataChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form className="space-y-6" onSubmit={handleFormSubmit}>
+      <div>
+        <label
+          htmlFor="email"
+          className="block text-sm/6 font-medium text-gray-900"
+        >
+          Email address
+        </label>
+        <div className="mt-2">
+          <input
+            id="email"
+            name="email"
+            type="email"
+            required
+            autoComplete="email"
+            value={formData.email}
+            onChange={handleDataChange}
+            className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+          />
+        </div>
+      </div>
+
+      <div>
+        <div className="flex items-center justify-between">
+          <label
+            htmlFor="password"
+            className="block text-sm/6 font-medium text-gray-900"
+          >
+            Password
+          </label>
+        </div>
+        <div className="mt-2">
+          <input
+            id="password"
+            name="password"
+            type="password"
+            required
+            autoComplete="current-password"
+            className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+            value={formData.password}
+            onChange={handleDataChange}
+          />
+        </div>
+      </div>
+
+      <div>
+        <button
+          type="submit"
+          className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Sign in
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default LoginForm;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import PageLayout from "../components/PageLayout";
+import LoginForm from "../components/LoginForm";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <PageLayout>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+            Sign in to your account
+          </h2>
+        </div>
 
-export default Login
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <LoginForm />
+        </div>
+      </div>
+    </PageLayout>
+  );
+};
+
+export default Login;
